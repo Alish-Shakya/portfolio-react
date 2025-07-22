b7a9e90ae104da6475d766c60095ff9b20f25f6e
Alish-Shakya <alishshakya44@gmail.com>
2025-07-22 22:47:02 +0545

    initial commit

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+      <NavLink to="/">Home</NavLink>
+    </>
+  );
+};
+
+export default Navbar;
